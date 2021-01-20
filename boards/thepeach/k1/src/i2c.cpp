425bcf7ef96ad89e5b95bce72cb2445038ca5edc
sugigoon <sugigoon@gmail.com>
2021-01-20 15:23:03 +0900

    THE PEACH K1 Board's I2C2 Issue fix

--- boards/thepeach/k1/src/i2c.cpp
+++ boards/thepeach/k1/src/i2c.cpp
@@ -31,8 +31,11 @@
  *
  ****************************************************************************/
 
+#include "board_config.h"
+#include <px4_platform_common/i2c.h>
 #include <px4_arch/i2c_hw_description.h>
 
 constexpr px4_i2c_bus_t px4_i2c_buses[I2C_BUS_MAX_BUS_ITEMS] = {
 	initI2CBusExternal(1),
+	initI2CBusInternal(2),
 };
